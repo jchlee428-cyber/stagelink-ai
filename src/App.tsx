0b5e7f78bff49172c68ba66afa0bf8d2db466bb6
이종철 <jchlee428@gmail.com>
2026-08-18 17:46:01 +0900

    fix(router): sanitize router initialization and BrowserRouter basename

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,11 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import { AuthProvider } from "./hooks/useAuth";
 
-
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <AuthProvider>
-        <BrowserRouter basename={__BASE_PATH__}>
+        <BrowserRouter>
           <AppRoutes />
         </BrowserRouter>
       </AuthProvider>
